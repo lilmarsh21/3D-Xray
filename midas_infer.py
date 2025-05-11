89e1e70c6a427c712b731ae3e42f6922e86ef5da
lilmarsh21 <hc3506@gmail.com>
2025-05-10 22:38:24 -0400

    Update midas_infer.py

--- midas_infer.py
+++ midas_infer.py
@@ -2,11 +2,30 @@ import torch
 from PIL import Image
 import cv2
 import numpy as np
+import os
+import requests
+
+MODEL_DIR = "models"
+MODEL_PATH = os.path.join(MODEL_DIR, "dpt_large-midas.pt")
+MODEL_URL = "https://huggingface.co/Visheratin/midas-dpt-large/resolve/main/dpt_large-midas.pt"
+
+def download_model():
+    os.makedirs(MODEL_DIR, exist_ok=True)
+    if not os.path.exists(MODEL_PATH):
+        print("⏬ Downloading MiDaS model...")
+        r = requests.get(MODEL_URL)
+        with open(MODEL_PATH, "wb") as f:
+            f.write(r.content)
+        print("✅ Model downloaded.")
 
 def load_midas_model():
+    download_model()
+
     model_type = "DPT_Large"
     midas = torch.hub.load("intel-isl/MiDaS", model_type, trust_repo=True)
+    midas.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
     midas.eval()
+
     transform = torch.hub.load("intel-isl/MiDaS", "transforms").dpt_transform
     return midas, transform
 
